0e32c028ac94de7f6be0c42ee1b55a6610ef7f5c
Waguirre00 <71514774+josue674@users.noreply.github.com>
2023-06-14 15:57:12 -0600

    V- 0.0.1

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import "./App.css";
+import { Recipe } from "./Consultas";
+import { BsGithub, BsTwitter } from "react-icons/bs";
+
+function App() {
+  const [data, setData] = useState({
+    instructions: "",
+    ingredients: "",
+  });
+  const [recipe, setRecipe] = useState("");
+  const apiKey = import.meta.env.VITE_APP_OPENAI_API_KEY;
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const consulta = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    Recipe(data, apiKey).then((res) => {
+      setRecipe(res);
+      setLoading(false);
+    });
+  };
+
+  return (
+    <>
+      <div className="App">
+        <div className="DivContentInformation">
+          <div className="DivContentInstru">
+            <label className="Textos">Instructions :</label>
+            <textarea
+              type="text"
+              placeholder="I don't want to use the oven"
+              className="InputInstru"
+              name="instructions"
+              onChange={(e) => {
+                handleInputChange(e);
+              }}
+            />
+          </div>
+          <div className="DivContentIngre">
+            <label className="Textos">Ingredients :</label>
+            <textarea
+              type="text"
+              placeholder="Cucumber, Tomato, Salt and Lettuce"
+              className="InputIngre"
+              name="ingredients"
+              onChange={(e) => {
+                handleInputChange(e);
+              }}
+            />
+          </div>
+          <button
+            {...(loading
+              ? { disabled: true, className: "ButtonLoad" }
+              : { className: "ButtonGenerate" })}
+            onClick={(e) => {
+              consulta(e);
+            }}
+          >
+            {loading ? "Loading..." : "Generate"}
+          </button>
+          <div className="DivContentCon">
+            <div className="DivContentDescri">
+              <label className="LabelTitle">Random Recipe</label>
+              <label className="LabelDescrip">
+                It is an open source random recipe generator that uses AI
+              </label>
+            </div>
+            <div className="DivContentBut">
+              <button
+                className="BTNGit"
+                onClick={() => {
+                  window.open("https://twitter.com/Feedbacks_dev", "_blank");
+                }}
+              >
+                <BsGithub />
+              </button>
+              <button
+                className="BTNTwi"
+                onClick={() => {
+                  window.open("https://twitter.com/Feedbacks_dev", "_blank");
+                }}
+              >
+                <BsTwitter />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="DivContentRecipe">
+          <label className="Textos">Recipe</label>
+          <textarea className="TextRecipe" readOnly value={recipe} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Consultas.js
@@ -0,0 +1,30 @@
+export async function Recipe(req, apiKey) {
+//   console.log(req);
+    // console.log(apiKey);
+  const prompt = `Write a recipe based on these ingredients and instructions:${req.instructions} Ingredients:${req.ingredients}`;
+  // console.log(JSON.stringify({prompt}));
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      prompt,
+      temperature: 0.5,
+      max_tokens: 4000,
+      n: 1,
+      stop: "\\n",
+      model: "text-davinci-003",
+      frequency_penalty: 0.5,
+      presence_penalty: 0.5,
+      logprobs: 10,
+    }),
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    console.log(data);
+    throw new Error(data.error.message);
+  }
+  return data.choices[0].text;
+}
